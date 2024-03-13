51315c03d5e43028700bcd9ff57b6fdb12176e88
Nisarg wath <94218703+NisargWath@users.noreply.github.com>
2024-03-13 16:36:47 +0530

    commit

---
+++ chatbot-template-main copy/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, jsonify
+import google.generativeai as genai
+
+
+
+app = Flask(__name__)
+GOOGLE_API_KEY = "AIzaSyDPAtGf-Rwe5nUQ_uDCiifRGVQxND5cB_c"
+genai.configure(api_key=GOOGLE_API_KEY)
+model = genai.GenerativeModel(model_name="gemini-pro")
+
+import markdown
+
+def to_markdown(text):
+    # Convert plain text to Markdown format
+    md = markdown.markdown(text)
+    return md
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    msg = request.form['message'] + " Give me in 150-200 words "
+    print(msg)
+    response = model.generate_content(msg) 
+    return jsonify({"message": to_markdown(response.text)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
